87484bf3e0fbc1ef64a4c9ea47323cf89ca1f9ae
oluwatobi ayoola jolaosho <fury25423@gmail.com>
2024-10-20 10:49:05 +0100

    fixed middlewares

---
+++ middleware/authmiddleware.js
@@ -0,0 +1,47 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../model/users/user");
+
+const protect = asyncHandler(async (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        try {
+            // Get token from header
+            token = req.headers.authorization.split(" ")[1];
+            //console.log(token)
+            // Verify token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            // Calculate the current time in seconds since Unix epoch
+            const currentTimeInSeconds = Math.floor(Date.now() / 1000);
+
+            // Calculate the time elapsed since token creation
+            const timeElapsedInSeconds = currentTimeInSeconds - decoded.iat;
+
+            // Check if more than 12 hours (12 hours * 60 minutes * 60 seconds)
+            if (timeElapsedInSeconds > 48 * 60 * 60) {
+                throw Object.assign(new Error("Session expired"), { statusCode: 401 });
+                ;
+            }
+
+            // Get user from the token
+            req.auth = await User.findById(decoded.id).select("-password");
+
+            next();
+        } catch (error) {
+            if (error.message === 'Session expired') {
+                throw Object.assign(new Error("Session expired"), { statusCode: 401 });
+
+            } else {
+                throw Object.assign(new Error("Not authorized"), { statusCode: 403 });
+                ;
+            }
+        }
+    } else {
+        throw Object.assign(new Error("Not authorized"), { statusCode: 403 });
+        ;
+    }
+});
+
+module.exports = { protect };
---
+++ middleware/errormiddleware.js
@@ -0,0 +1,26 @@
+const { stack } = require("../routes/users");
+const logger = require("../utils/logger.js");
+
+const errorHandler = (err, req, res, next) => {
+  let statusCode;
+  if (err.statusCode || res.statusCode) {
+    statusCode = err.statusCode ? err.statusCode : res.statusCode;
+  } else {
+    statusCode = 500;
+  }
+  const errorMessage = err.message
+    ? err.message.startsWith("Error: ")
+      ? err.message.slice(7)
+      : err.message
+    : process.env.NODE_ENV === "Production"
+    ? "Server Error"
+    : "Something went wrong";
+
+  res.status(statusCode).json({
+    Message: errorMessage,
+  });
+  next();
+};
+module.exports = {
+  errorHandler,
+};
