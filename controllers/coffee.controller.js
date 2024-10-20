e009fd8e35efb1642417d7d87b21eb314fe53f66
oluwatobi ayoola jolaosho <fury25423@gmail.com>
2024-10-20 12:21:32 +0100

    finished basic shop controller

--- controllers/coffee.controller.js
+++ controllers/coffee.controller.js
@@ -476,28 +476,32 @@ const user = await users.findById(id)
 
 
 const deleteShop = asynchandler(async (req, res) => {
-  const { shopId } = req.params;
+  const { coffeeId } = req.params;
   const { id } = req.auth;
 
   try {
-    if (!shopId) {
+    if (!coffeeId) {
       throw Object.assign(new Error("Shop ID is empty"), {
         statusCode: 400,
       });
     }
 
-    const shop = await COFFEE.findById(shopId);
-    if (!shop) {
+    const coffee = await COFFEE.findById(shopId);
+    if (!coffee) {
       throw Object.assign(new Error("Shop not found"), {
         statusCode: 404,
       });
     }
 
-    if (!(shop.owner.toString() === id)) {
-      throw Object.assign(new Error("Not authorized"), {
-        statusCode: 403,
-      });
-    }
+      const user = await users.findById(id)
+      if (
+          !(
+              user.role === "admin"
+          )
+      ) {
+          throw Object.assign(new Error("not authorized"), {
+              statusCode: 403,
+          }
 
     const deletedShop = await COFFEE.findByIdAndDelete(shopId);
 
@@ -506,19 +510,10 @@ const deleteShop = asynchandler(async (req, res) => {
         statusCode: 500,
       });
     }
-
-    // Delete associated working hours or other related data if needed
-    // ...
-
     res.status(200).json({
       success: true,
     });
 
-    logger.info(
-      `User with id ${id} deleted shop with id: ${shopId} at ${new Date()} - ${
-        res.statusCode
-      } - ${res.statusMessage} - ${req.originalUrl} - ${req.method} - ${req.ip}`
-    );
   } catch (error) {
     throw Object.assign(new Error("Banned from forum"), {
       statusCode: error.statusCode,
@@ -526,398 +521,7 @@ const deleteShop = asynchandler(async (req, res) => {
   }
 });
 
-const getLocation = asynchandler(async (ip) => {
-  try {
-    // Set endpoint and your access key
-    const accessKey = process.env.ip_secret_key;
-    const url =
-      "http://apiip.net/api/check?ip=" + ip + "&accessKey=" + accessKey;
 
-    // Make a request and store the response
-    const response = await fetch(url);
-
-    // Decode JSON response:
-    const result = await response.json();
-
-    // Output the "code" value inside "currency" object
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    return null;
-  }
-});
-/**
- * @api {put} /updateapp/:shopId Update Shop Approval Status
- * @apiName UpdateShopApproval
- * @apiGroup Shop
- *
- * @apiHeader {String} Authorization User's authorization token.
- * @apiParam {String} shopId ID of the shop to update.
- * @apiParam {Boolean} status New approval status.
- *
- * @apiSuccess {Object} data Updated shop object.
- * @apiSuccess {String} token Authorization token.
- *
- * @apiSuccessExample Success-Response:
- *     HTTP/1.1 200 OK
- *     {
- *       "data": {
- *         "_id": "shopId",
- *         "name": "shopName",
- *         "approved": true,
- *         // other updated shop fields
- *       },
- *       "token": "authorizationToken"
- *     }
- *
- * @apiError (401) NotAuthorized The user is not authorized to update this data.
- * @apiError (404) NotAUser The user was not found.
- *
- * @apiErrorExample Error-Response:
- *     HTTP/1.1 401 Unauthorized
- *     {
- *       "error": "NotAuthorized"
- *     }
- *
- * @apiErrorExample Error-Response:
- *     HTTP/1.1 404 Not Found
- *     {
- *       "error": "NotAUser"
- *     }
- */
-
-const updateapproval = asynchandler(async (req, res) => {
-  try {
-    const page = parseInt(req.query.page) || 1;
-    const pageSize = parseInt(req.query.pageSize) || 10;
-    const { id } = req.auth;
-    const { shopId } = req.params;
-    const { status } = req.body;
-    const shop = await COFFEE.findById(shopId);
-    const user = await USER.findById(id);
-    if (
-      !(
-        shop.owner.toString() === id ||
-        process.env.role.toString() === "superadmin" ||
-        user.role === "superadmin"
-      )
-    )
-      throw Object.assign(new Error("Not authorized"), { statusCode: 401 });
-    const updatedUser = await COFFEE.findByIdAndUpdate(
-      shopId,
-      { $set: { approved: status } },
-      { new: true }
-    );
-
-    if (!updatedUser) {
-      throw Object.assign(new Error("Not a user"), { statusCode: 404 });
-    }
-    const token = generateToken(id);
-    res.status(200).header("Authorization", `Bearer ${token}`).json({
-      data: updatedUser,
-    });
-    logger.info(
-      `shop with id ${shopId} was updated by user with id ${id} - ${res.statusCode} - ${res.statusMessage} - ${req.originalUrl} - ${req.method} - ${req.ip} - from ${req.ip} `
-    );
-  } catch (error) {
-    console.log(error);
-    throw Object.assign(new Error(`${error}`), {
-      statusCode: error.statusCode,
-    });
-  }
-});
-/**
- * @api {put} /updatesub/:shopId Update Shop Subscription Status
- * @apiName UpdateShopSubscription
- * @apiGroup Shop
- *
- * @apiHeader {String} Authorization User's authorization token.
- * @apiParam {String} shopId ID of the shop to update.
- * @apiParam {Boolean} status New subscription status.
- *
- * @apiSuccess {Object} data Updated shop object.
- * @apiSuccess {String} token Authorization token.
- *
- * @apiSuccessExample Success-Response:
- *     HTTP/1.1 200 OK
- *     {
- *       "data": {
- *         "_id": "shopId",
- *         "name": "shopName",
- *         "subscribed": true,
- *         // other updated shop fields
- *       },
- *       "token": "authorizationToken"
- *     }
- *
- * @apiError (401) NotAuthorized The user is not authorized to update this data.
- * @apiError (404) UserNotFound The user was not found.
- *
- * @apiErrorExample Error-Response:
- *     HTTP/1.1 401 Unauthorized
- *     {
- *       "error": "NotAuthorized"
- *     }
- *
- * @apiErrorExample Error-Response:
- *     HTTP/1.1 404 Not Found
- *     {
- *       "error": "UserNotFound"
- *     }
- */
-const updatesubscription = asynchandler(async (req, res) => {
-  try {
-    const page = parseInt(req.query.page) || 1;
-    const pageSize = parseInt(req.query.pageSize) || 10;
-    const { id } = req.auth;
-    const { shopId } = req.params;
-    const { status } = req.body;
-    const shop = await COFFEE.findById(shopId);
-    const user = await USER.findById(id);
-    if (
-      !(
-        shop.owner.toString() === id ||
-        process.env.role.toString() === "superadmin" ||
-        user.role === "superadmin"
-      )
-    )
-      throw Object.assign(new Error("Not authorized"), { statusCode: 401 });
-    const updatedUser = await COFFEE.findByIdAndUpdate(
-      shopId,
-      { $set: { subscribed: status } },
-      { new: true }
-    );
-
-    if (!updatedUser) {
-      throw Object.assign(new Error("User not found"), { statusCode: 404 });
-    }
-
-    const token = generateToken(id);
-    res.status(200).header("Authorization", `Bearer ${token}`).json({
-      data: updatedUser,
-    });
-    logger.info(
-      `admin with id ${id}, updated shop with id ${shopId} - ${res.statusCode} - ${res.statusMessage} - ${req.originalUrl} - ${req.method} - ${req.ip} - from ${location} `
-    );
-  } catch (error) {
-    throw Object.assign(new Error(`${error}`), {
-      statusCode: error.statusCode,
-    });
-  }
-});
-/**
- * @api {put} /updateavb/:shopId Update Shop Availability Status
- * @apiName UpdateShopAvailability
- * @apiGroup Shop
- *
- * @apiHeader {String} Authorization User's authorization token.
- * @apiParam {String} shopId ID of the shop to update.
- * @apiParam {Boolean} status New availability status.
- *
- * @apiSuccess {Object} data Updated shop object.
- * @apiSuccess {String} token Authorization token.
- *
- * @apiSuccessExample Success-Response:
- *     HTTP/1.1 200 OK
- *     {
- *       "data": {
- *         "_id": "shopId",
- *         "name": "shopName",
- *         "avalabilty": true,
- *         // other updated shop fields
- *       },
- *       "token": "authorizationToken"
- *     }
- *
- * @apiError (401) NotAuthorized The user is not authorized to update this data.
- * @apiError (404) UserNotFound The user was not found.
- *
- * @apiErrorExample Error-Response:
- *     HTTP/1.1 401 Unauthorized
- *     {
- *       "error": "NotAuthorized"
- *     }
- *
- * @apiErrorExample Error-Response:
- *     HTTP/1.1 404 Not Found
- *     {
- *       "error": "UserNotFound"
- *     }
- */
-
-const updateavalability = asynchandler(async (req, res) => {
-  try {
-    const page = parseInt(req.query.page) || 1;
-    const pageSize = parseInt(req.query.pageSize) || 10;
-    const { id } = req.auth;
-    const { shopId } = req.params;
-    const { status } = req.body;
-    const shop = await COFFEE.findById(shopId);
-    const user = await USER.findById(id);
-    if (
-      !(
-        shop.owner.toString() === id ||
-        process.env.role.toString() === "superadmin" ||
-        user.role === "superadmin"
-      )
-    )
-      throw Object.assign(new Error("Not authorized"), { statusCode: 401 });
-
-    const updatedUser = await COFFEE.findByIdAndUpdate(
-      shopId,
-      { $set: { avalabilty: status } },
-      { new: true }
-    );
-
-    if (!updatedUser) {
-      throw Object.assign(new Error("User not found"), { statusCode: 404 });
-    }
-
-    const token = generateToken(id);
-    res.status(200).header("Authorization", `Bearer ${token}`).json({
-      data: updatedUser,
-    });
-    logger.info(
-      `admin with id ${id}, updated shop with id ${shopId} avalability - ${res.statusCode} - ${res.statusMessage} - ${req.originalUrl} - ${req.method} - ${req.ip} - from ${location} `
-    );
-  } catch (error) {
-    throw new Error(`${error}`);
-  }
-});
-
-/**
- * @api {get} /search Search Shops
- * @apiName SearchShops
- * @apiGroup Shop
- *
- * @apiParam {String} query Search query.
- *
- * @apiSuccess {Array} data Array of shop objects that match the search query.
- *
- * @apiSuccessExample Success-Response:
- *     HTTP/1.1 200 OK
- *     {
- *       "data": [
- *         {
- *           "_id": "shopId",
- *           "name": "shopName",
- *           // other shop fields
- *         },
- *         // more shop objects
- *       ]
- *     }
- *
- * @apiError (500) ServerError An error occurred on the server.
- *
- * @apiErrorExample Error-Response:
- *     HTTP/1.1 500 Internal Server Error
- *     {
- *       "error": "ServerError"
- *     }
- */
-const searchShops = asynchandler(async (req, res) => {
-  const query = req.query.query;
-  try {
-    const shopResults = await COFFEE.find({ $text: { $search: query } }).sort({
-      createdAt: -1,
-    });
-    res.status(200).json({
-      data: shopResults,
-    });
-
-    logger.info(
-      `Shop search results fetched - ${res.statusCode} - ${res.statusMessage} - ${req.originalUrl} - ${req.method} - ${req.ip} - from ${req.ip}`
-    );
-  } catch (error) {
-    console.error(error);
-    throw Object.assign(new Error(`${error}`), {
-      statusCode: error.statusCode,
-    });
-  }
-});
-
-/**
- * @api {put} /updateservices/:shopId Update Shop Services
- * @apiName UpdateShopServices
- * @apiGroup Shop
- *
- * @apiHeader {String} Authorization User's authorization token.
- * @apiParam {String} shopId ID of the shop to update.
- * @apiParam {Array} services Array of services offered by the shop.
- *
- * @apiSuccess {String} status Status of the request.
- * @apiSuccess {Object} data Updated shop object.
- * @apiSuccess {String} token Authorization token.
- *
- * @apiSuccessExample Success-Response:
- *     HTTP/1.1 200 OK
- *     {
- *       "status": "success",
- *       "data": {
- *         "_id": "shopId",
- *         "name": "shopName",
- *         "servicesOffered": ["service1", "service2"],
- *         // other updated shop fields
- *       },
- *       "token": "authorizationToken"
- *     }
- *
- * @apiError (401) NotAuthorized The user is not authorized to update this data.
- * @apiError (404) NoShopsFound No shops were found.
- * @apiError (422) InvalidData The provided data is invalid.
- *
- * @apiErrorExample Error-Response:
- *     HTTP/1.1 401 Unauthorized
- *     {
- *       "error": "NotAuthorized"
- *     }
- *
- * @apiErrorExample Error-Response:
- *     HTTP/1.1 404 Not Found
- *     {
- *       "error": "NoShopsFound"
- *     }
- *
- * @apiErrorExample Error-Response:
- *     HTTP/1.1 422 Unprocessable Entity
- *     {
- *       "error": "InvalidData"
- *     }
- */
-const updateServices = asynchandler(async (req, res) => {
-  try {
-    const { id } = req.auth;
-    const { shopId } = req.params;
-    const { services } = req.body;
-    if (id !== shop.owner)
-      throw Object.assign(new Error("Not authorized"), { statusCode: 401 });
-    if (!servicesOffered || !Array.isArray(servicesOffered)) {
-      throw Object.assign(new Error("Invalid data"), { statusCode: 422 });
-    }
-
-    const updatedShop = await COFFEE.findByIdAndUpdate(
-      shopId,
-      { $set: { servicesOffered: services.split(",") } },
-      { new: true }
-    );
-
-    if (!updatedShop) {
-      throw Object.assign(new Error("No shops found"), { statusCode: 404 });
-    }
-
-    const token = generateToken(updatedShop.owner);
-    res.status(200).header("Authorization", `Bearer ${token}`).json({
-      status: "success",
-      data: updatedShop,
-    });
-
-    logger.info(
-      `Services for shop with id: ${shopId} updated by user with id ${req.auth.id} - ${res.statusCode} - ${res.statusMessage} - ${req.originalUrl} - ${req.method} - ${req.ip}`
-    );
-  } catch (error) {
-    throw Object.assign(new Error(`${error}`), { statusCode: 404 });
-  }
-});
 
 const generateToken = (id) => {
   return jwt.sign(
@@ -929,19 +533,5 @@ const generateToken = (id) => {
   );
 };
 module.exports = {
-  create_shops,
-  getallshops,
-  updateShops,
-  login_shops,
-  getallshopone,
-  updateWorkingHours,
-  updateapproval,
-  updatesubscription,
-  searchShops,
-  getshop,
-  getall,
-  deleteShop,
-  updateavalability,
-  consentToUserAgreement,
-  updateServices,
+
 };
